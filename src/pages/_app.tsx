5c3f7155ba2d41dbbfe6744abaa03be2276d768d
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-03-31 02:14:53 -0300

    cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,17 @@ import type { AppProps } from 'next/app'
 
 globalStyles() //chamando a função de estilo global
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
