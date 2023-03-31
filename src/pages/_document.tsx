2dc307340d484f56e8b3cd9a5fde3ade37495e61
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-03-31 01:53:26 -0300

    configurando css in js Stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from '@/styles/styles'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -5,13 +6,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
-            </Head>
-            <body>
-              <Main />
-              <NextScript />
-            </body>
-          </Html>
-          )
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
+
+        <style id='stitches' dangerouslySetInnerHTML={{ __html: getCssText() }} /> {/* permitindo que o CssInJs tmb seja renderizado no lado do servidor */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
