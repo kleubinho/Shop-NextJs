4eb433aa1b13f48b62dd2dea91b1e539d1e8d759
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-04-03 14:55:19 -0300

    imagens otimizadas no next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,13 @@ globalStyles() //chamando a função de estilo global
 
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
