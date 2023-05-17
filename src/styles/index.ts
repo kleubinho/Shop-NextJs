fb148c41d001e671538fc555c5dd061d09966cc8
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-05-17 16:06:30 -0300

    updaye

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -8,21 +8,28 @@ export const {
   getCssText,
   theme,
   createTheme,
-  css
+  css,
 } = createStitches({
   theme: {
     colors: {
-      white: '#FFF',
+      white: "#FFF",
 
-      gray900: '#121214',
-      gray800: '#202024',
-      gray300: '#c4c4cc',
-      gray100: '#e1e1e6',
+      gray900: "#121214",
+      gray800: "#202024",
+      gray300: "#c4c4cc",
+      gray100: "#e1e1e6",
 
-      green500: '#00075f',
-      green300: '#00b37e',
+      green500: "#00075f",
+      green300: "#00b37e",
 
       rocketseat: "#8257e6",
+    },
+
+    fontSizes: {
+      md: "1.125rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "2rem",
     },
   },
 });
